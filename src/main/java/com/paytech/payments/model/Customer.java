1c4bca5111c146363923e71c56319772468fbc19
dariakl <dasha.klochkova.00@gmail.com>
2024-08-26 22:28:09 +0300

    Add pom.xml

--- src/main/java/com/paytech/payments/model/Customer.java
+++ src/main/java/com/paytech/payments/model/Customer.java
@@ -1,25 +1,14 @@
 package com.paytech.payments.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Customer {
 
-    private String referenceId = "customer_123";
-    private String citizenshipCountryCode = "AU";
-    private String firstName = "John";
-    private String lastName = "Smith";
-    private String dateOfBirth = "2001-12-03";
-    private String email = "my@email.com";
-    private String phone = "357 123123123";
-    private String locale = "ru";
-    private String accountNumber = "string";
-    private String accountName = "string";
-    private String bank = "string";
-    private String bankBranch = "string";
-    private String documentType = "string";
-    private String documentNumber = "string";
-    private String routingGroup = "VIP";
-    private boolean kycStatus = true;
-    private boolean paymentInstrumentKycStatus = true;
+    private String referenceId = "customer_124";
+    private String citizenshipCountryCode = "RU";
+    private String firstName = "Daria";
+    private String lastName = "K";
 }
